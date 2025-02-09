aeb36bb7f7082d822472a464384797258315f2fa
Jorge Mendoza <jorgevmendoza@gmail.com>
2025-02-09 09:49:29 +0100

    Version 1.0 of Apple Association File

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+// Define the AASA JSON structure
+const appleAppSiteAssociation = {
+    "applinks": {
+        "details": [
+            {
+                "appID": ["7G2PMCVCH6.io.dynamiteapps.paimi"],
+                "paths": [
+                    "/link/*"
+                ]
+            }
+        ]
+    }
+};
+
+// Middleware to set correct Content-Type
+const setJSONHeader = (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+};
+
+// Serve AASA at both paths
+app.get('/.well-known/apple-app-site-association', setJSONHeader, (req, res) => {
+    res.send(appleAppSiteAssociation);
+});
+
+app.get('/apple-app-site-association', setJSONHeader, (req, res) => {
+    res.send(appleAppSiteAssociation);
+});
+
+// Start the Express server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on:${PORT}`);
+});
