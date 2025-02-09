7e1fd4783524dd99dd75c0821449ff853b1b09ea
Jorge Mendoza <jorgevmendoza@gmail.com>
2025-02-09 10:46:25 +0100

    re-added paths to file

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const appleAppSiteAssociation = {
                     "/": "/?code",
                     "comment": "Payment link"
                 }],
+                paths: ["/link/*"]
             }
         ]
     }
